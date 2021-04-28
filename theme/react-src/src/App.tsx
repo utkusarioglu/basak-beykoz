95d4752e3b46ac1db78c23ea7e3ffc4ada39c247
utkusarioglu <utkusarioglu@hotmail.com>
2021-04-28 05:59:44 +0000

    Update: remove React bloat
    * Removed React styles and other trivia

--- theme/react-src/src/App.tsx
+++ theme/react-src/src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h2 dangerouslySetInnerHTML={{ __html: window.config.pageTitle }} />
         <a href="/wp-admin">visit wp-admin</a>
       </header>
