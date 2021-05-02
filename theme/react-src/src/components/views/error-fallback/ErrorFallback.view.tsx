ec9a07a14578dcf95cb27549f5378ee9381b4f08
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-02 15:11:15 +0000

    Feat: add ErrorFallback.view
    * This component will be displayed if there is an error

---
+++ theme/react-src/src/components/views/error-fallback/ErrorFallback.view.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ErrorFallbackView = () => {
+  return (
+    <h1>
+      SOMETHING IS WRONG!1!!{' '}
+      <span role="img" aria-label="shocked">
+        😱
+      </span>
+    </h1>
+  );
+};
+
+export default ErrorFallbackView;
