63887e32584e1808d08591d2e07f79f8255034c7
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-02 12:30:59 +0000

    Update: singular render logic
    * Before this, there was no route fetch logic. Unless the user clicked
    the nav items, nothing appeared on the page. This has now been fixed.
    
    * Layout component has been updated to be aware of the non-render
    state of the singular. From now on, for the layout, singular
    is always in render state.

--- theme/react-src/src/components/layouts/singular/Singular.layout.tsx
+++ theme/react-src/src/components/layouts/singular/Singular.layout.tsx
@@ -2,31 +2,27 @@ import React from 'react';
 import type { FC } from 'react';
 import SingularView from '../../views/singular/Singular.view';
 import CategoryPostsFeedView from '../../views/category-posts-feed/CategoryPostsFeed.view';
-import type { SingularSliceState } from '../../../slices/singular/singular.slice.types';
+import type { SingularSliceRenderEnabled } from '../../../slices/singular/singular.slice.types';
 
 interface SingularLayoutProps {
-  singular: SingularSliceState;
+  singular: SingularSliceRenderEnabled;
 }
 
 const SingularLayout: FC<SingularLayoutProps> = ({ singular }) => {
   return (
     <>
-      {singular.render && (
-        <>
-          <h2>{singular.title}</h2>
-          <SingularView {...singular} />
+      <h2>{singular.title}</h2>
+      <SingularView {...singular} />
 
-          {singular.type === 'post' && (
-            <>
-              <h3>
-                Daha fazlası{' '}
-                <span role="img" aria-label="love">
-                  💙
-                </span>
-              </h3>
-              <CategoryPostsFeedView slug={singular.category} />
-            </>
-          )}
+      {singular.type === 'post' && (
+        <>
+          <h3>
+            Daha fazlası{' '}
+            <span role="img" aria-label="love">
+              💙
+            </span>
+          </h3>
+          <CategoryPostsFeedView slug={singular.category} />
         </>
       )}
     </>
--- theme/react-src/src/components/routes/singular/Singular.route.tsx
+++ theme/react-src/src/components/routes/singular/Singular.route.tsx
@@ -1,28 +1,52 @@
 import React from 'react';
+import { useEffect } from 'react';
 import SingularLayout from '../../layouts/singular/Singular.layout';
+import { cleanSlug } from '../../../utils/slug.util';
+import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectSingular } from '../../../slices/singular/singular.slice';
 import { HOME_SLUG } from '../../../config';
 import ReactGA from 'react-ga';
 import { Helmet } from 'react-helmet';
+import prefetch from '../../../services/prefetch.service';
+import { setIsLoading } from '../../../slices/app/app.slice';
 
 const SingularRoute = () => {
+  const { slug } = useParams<{ slug: string }>();
+  const cleanedSlug = slug !== undefined ? slug : 'home';
   const { pageTitle, titleSeparator, pageTagline } = window.config;
+  const singular = useSelector(selectSingular);
   const { pathname, search } = window.location;
   ReactGA.pageview(pathname + search);
-  const singular = useSelector(selectSingular);
+
+  useEffect(() => {
+    if (!singular.render) {
+      // setIsLoading(true);
+      prefetch.singular(cleanedSlug);
+      // .then(() => setIsLoading(false));
+    }
+  }, []);
 
   return (
     <>
-      {singular.render && (
-        <Helmet>
-          <title>
-            {pageTitle} {titleSeparator}{' '}
-            {singular.slug === HOME_SLUG ? pageTagline : singular.title}
-          </title>
-        </Helmet>
+      {singular.render ? (
+        <>
+          <Helmet>
+            <title>
+              {pageTitle} {titleSeparator}{' '}
+              {singular.slug === HOME_SLUG ? pageTagline : singular.title}
+            </title>
+          </Helmet>
+          <SingularLayout singular={singular} />
+        </>
+      ) : (
+        <div>
+          <h1 role="img" aria-label="cat" style={{ textAlign: 'center' }}>
+            😺
+          </h1>
+          <h4 style={{ textAlign: 'center' }}>Kedi sayfayı yüklüyor...</h4>
+        </div>
       )}
-      <SingularLayout singular={singular} />
     </>
   );
 };
