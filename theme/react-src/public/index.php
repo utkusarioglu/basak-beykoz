0b5cdea842693e514107b73b591400cb48db2ef2
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-02 11:09:23 +0000

    Update: index.php to reflect client spirit
    * Add metadata to index.php
    
    * Add a joyful loading text for index.php

--- theme/react-src/public/index.php
+++ theme/react-src/public/index.php
@@ -9,32 +9,50 @@
 <head>
 <meta charset="utf-8" />
   <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta name="theme-color" content="#000000" />
-  <meta
-    name="description"
-    content="Web site created using create-react-wptheme"
-  />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+    />
+  <meta name="theme-color" content="#505050" />
+  <meta name="description" content="Profesyonel koc Basak Beykoz'un internet sitesi" />
   <link rel="apple-touch-icon" href="<?php echo $TEMPLATE_PATH; ?>/logo192.png" />
-  <!--
-    manifest.json provides metadata used when your web app is installed on a
-    user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
-  -->
   <link rel="manifest" href="<?php echo $TEMPLATE_PATH; ?>/manifest.json" />
-  <!--
-    If you're reading this from "view source" in your browser, it might not make sense as
-    these tokens have already been evaluated and replaced, even in this remark blurb.
-
-    Notice the use of "php echo $TEMPLATE_PATH;" and %PUBLIC_URL% in the tags above.
-    Both will be replaced with the URL of the `public` folder during the build (%PUBLIC_URL%) or
-    at render time (php echo $TEMPLATE_PATH;)
-    Only files inside the `public` folder can be referenced like this.
-
-    Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
-    work correctly both with client-side routing and a non-root public URL.
-    Learn how to configure a non-root public URL by running `npm run wpbuild`.
-  -->
   <title><?php echo $PAGE_TITLE . " " . $TITLE_SEPARATOR .  " " . $PAGE_TAGLINE ?></title>
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="theme-color" content="#ffffff">
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Nunito&family=Raleway:wght@500&display=swap');
+
+    h1, h2, h3, h4, h5, h6 {
+      font-family: 'Raleway', sans-serif;
+    }
+
+    p, a, span, div {
+      font-family: 'Nunito', sans-serif;
+      color: black;
+      text-decoration: none;
+    }
+
+    body {
+      margin: 0;
+      padding; 0;
+    }
+
+    .singular-view a {
+      color: #005f35;
+    }
+
+    .singular-view a:hover {
+      text-decoration: underline;
+      text-decoration-thickness: 4px;
+      text-decoration-color: rgb(0 95 53 / 63%);
+    }
+  </style>
   <script>
     window.config = {
       pageTitle: '<?php echo $PAGE_TITLE ?>',
@@ -47,16 +65,8 @@
   <noscript>
     You need to enable JavaScript to run this app.
   </noscript>
-  <div id="root"></div>
-  <!--
-    This PHP file is a template.
-    If you open it directly in the browser, you will see an empty page.
-
-    You can add webfonts, meta tags, or analytics to this file.
-    The build step will place the bundled scripts into the <body> tag.
-
-    To begin the development, run `npm run wpstart` or `yarn wpstart`.
-    To create a production bundle, use `npm run wpbuild` or `yarn wpbuild`.
-  -->
+  <div id="root">
+    <div style="text-align: center; margin-top: 50px;">🦒 Mutlulukla yüklüyor... </div>
+  </div>
   </body>
 </html>
