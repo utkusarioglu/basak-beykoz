c50547bdf789146e7ed94eee177e33dbce5a9971
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-02 10:17:54 +0000

    Fix: isloading undefined return issue
    * IsLoading.view was returning undefined when the app wasn't loading,
    which was causing issues with the consuming component. This commit
    fixes the issue

--- theme/react-src/src/components/views/is-loading/IsLoading.view.tsx
+++ theme/react-src/src/components/views/is-loading/IsLoading.view.tsx
@@ -6,26 +6,24 @@ const IsLoadingView = () => {
   const isLoading = useSelector(selectIsLoading);
   const loadingText = 'Yüklüyor...';
 
-  return (
-    isLoading && (
-      <div
-        style={{
-          position: 'fixed',
-          top: 0,
-          right: 0,
-          left: 0,
-          height: '20px',
-          backgroundColor: '#005f35',
-          opacity: 0.8,
-          color: 'white',
-          zIndex: 100,
-          textAlign: 'center',
-        }}
-      >
-        {loadingText}
-      </div>
-    )
-  );
+  return isLoading ? (
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        right: 0,
+        left: 0,
+        height: '20px',
+        backgroundColor: '#005f35',
+        opacity: 0.8,
+        color: 'white',
+        zIndex: 100,
+        textAlign: 'center',
+      }}
+    >
+      {loadingText}
+    </div>
+  ) : null;
 };
 
 export default IsLoadingView;
