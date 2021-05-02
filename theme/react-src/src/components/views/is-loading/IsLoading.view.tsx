2e146a60421fd15d6880ee697c86aa82ba25bba3
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-02 10:14:57 +0000

    Feat: add basic IsLoading.view
    * add a basic IsLoading.view component

---
+++ theme/react-src/src/components/views/is-loading/IsLoading.view.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectIsLoading } from '../../../slices/app/app.slice';
+
+const IsLoadingView = () => {
+  const isLoading = useSelector(selectIsLoading);
+  const loadingText = 'Yüklüyor...';
+
+  return (
+    isLoading && (
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          right: 0,
+          left: 0,
+          height: '20px',
+          backgroundColor: '#005f35',
+          opacity: 0.8,
+          color: 'white',
+          zIndex: 100,
+          textAlign: 'center',
+        }}
+      >
+        {loadingText}
+      </div>
+    )
+  );
+};
+
+export default IsLoadingView;
