b5d5e22951307929222ab473c24d73ebb1b5055e
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-02 07:56:42 +0000

    Feat: add utils for slug
    * Slugs will require manipulation to adapt to url, the rest requests
    and state comparisons. This util is for manipulating the slug content
    depending on the need.

---
+++ theme/react-src/src/utils/slug.util.tsx
@@ -0,0 +1,35 @@
+import { HOME_SLUG } from '../config';
+
+type StringIdentityFunc = (slug: string) => string;
+
+/**
+ * Converts an empty or undefined slug to one with a slash at the end
+ * @param slug slug to convert to url compatible format
+ * @returns address bar routing compatible slug
+ */
+export const urlSlug: StringIdentityFunc = (slug) =>
+  slug !== undefined && slug !== '' ? `/${slug}` : '/';
+
+/**
+ * Converts raw slug to values that the rest api would understand.
+ * This involved getting rid of empty and undefined values and also converting
+ * '/' to '/home' or rather HOME_SLUG value from env.
+ * @param slug slug to convert to restful api compatible format
+ * @returns rest api compatible slug
+ */
+export const restSlug: StringIdentityFunc = (slug) =>
+  (slug !== undefined && slug !== '' && slug !== '/'
+    ? `/${slug}`
+    : `/${HOME_SLUG}`
+  ).replace('//', '/');
+
+/**
+ * Removes the preceding slash from the given slug, it also converts
+ * any undefined value to HOME_SLUG value from env.
+ * @param rawSlug slug that may have a slash at the beginning
+ * @returns slug that could be used for state comparison
+ */
+export const cleanSlug: StringIdentityFunc = (rawSlug) => {
+  const slashlessSlug = rawSlug.replace('/', '');
+  return rawSlug === undefined ? HOME_SLUG : slashlessSlug;
+};
