28cbae8d1b033349fc07bfd3378f83f475fddd92
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-02 10:04:01 +0000

    Feat: add Social.view
    * add Social.view component for displaying client's social links

---
+++ theme/react-src/src/components/views/social/Social.view.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { AiFillLinkedin, AiFillInstagram } from 'react-icons/ai';
+
+const socialItems = [
+  {
+    title: 'Instagram',
+    icon: <AiFillInstagram size="1.3em" />,
+    link: 'https://www.instagram.com/basak_beykoz/',
+  },
+  {
+    title: 'Linkedin',
+    link: 'https://www.linkedin.com/in/basakbeykoz/',
+    icon: <AiFillLinkedin size="1.3em" />,
+  },
+];
+
+const SocialView = () => {
+  return (
+    <div>
+      {socialItems.map(({ title, link, icon }) => (
+        <a
+          key={title}
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          title={title}
+          style={{ marginLeft: '0.8em' }}
+        >
+          {icon}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default SocialView;
