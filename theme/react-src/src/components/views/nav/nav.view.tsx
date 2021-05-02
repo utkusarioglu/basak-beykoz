894ab0876a95cda52e70d7de2a8eb6a97628643b
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-02 09:43:10 +0000

    Feat: add nav.view component for navigation

---
+++ theme/react-src/src/components/views/nav/nav.view.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import type { WpMenuItem, GetWpMenuSuccess } from '../../../@types/wp-types';
+import rest from '../../../services/rest.service';
+import NavItemView from '../nav-item/NavItem.view';
+
+const NavView = () => {
+  const [menu, setMenu] = useState<WpMenuItem[]>([]);
+
+  useEffect(() => {
+    rest.fetchMenu('nav').then((data) => {
+      if (data.hasOwnProperty('code')) {
+        setMenu([]);
+      } else {
+        setMenu((data as GetWpMenuSuccess).items);
+      }
+    });
+  }, []);
+
+  return (
+    <nav
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        position: 'relative',
+      }}
+    >
+      {menu.length > 0
+        ? menu.map((menuItem) => (
+            <p>{menuItem.title}</p>
+          ))
+        : null}
+    </nav>
+  );
+};
+
+export default NavView;
