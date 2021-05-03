10c6277dbb64b3551fc2acfc62b451614f185a75
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-03 15:14:39 +0000

    Feat: add MenuLoader.view
    * This component is the fallback for menu loading

---
+++ theme/react-src/src/components/views/menu-loader/MenuLoader.view.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <p style={{ textAlign: 'center' }}>
+      <span role="img" aria-label="loading">
+        😺
+      </span>
+      Kedi yine is başında
+    </p>
+  </div>
+);
