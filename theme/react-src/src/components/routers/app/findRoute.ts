3613fbc320a1f0513d5ee0ae85b3f1137e948742
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-03 11:41:17 +0000

    Chore: remove console.log

--- theme/react-src/src/components/routers/app/findRoute.ts
+++ theme/react-src/src/components/routers/app/findRoute.ts
@@ -1,7 +1,6 @@
 import { routes } from './routeConfig';
 
 export function findRoute(restfulSlug: string) {
-  console.log('restful slug', restfulSlug);
   return (
     routes.find((route) => route.path === restfulSlug) ||
     routes.find((route) => route.path === '/:slug')
