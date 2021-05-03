f7d9ff22c4bc9051de551649848482cd4ba1febd
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-03 15:35:05 +0000

    Feat: add basic MobileNavView
    * The component works and displays the nav items, but it does so with
    the expectation of a cursor being available. This error shall be
    corrected in a future commit.

--- theme/react-src/src/components/views/mobile-nav/MobileNav.view.tsx
+++ theme/react-src/src/components/views/mobile-nav/MobileNav.view.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import NavItemView from '../nav-item/NavItem.view';
+import { useSelector } from 'react-redux';
+import { selectNav } from '../../../slices/nav/nav.slice';
+import prefetch from '../../../services/prefetch.service';
+import MenuLoaderView from '../menu-loader/MenuLoader.view';
 
-const MobileNavMenu = () => {
-  return <div>This will be the nav menu</div>;
+const MobileNavView = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { items: menu } = useSelector(selectNav);
+
+  useEffect(() => {
+    prefetch.menu({
+      slug: 'nav',
+      onFetchStart: () => setIsLoading(true),
+      onFetchComplete: () => setIsLoading(false),
+    });
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <MenuLoaderView />
+      ) : (
+        <nav
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            position: 'relative',
+            textAlign: 'right',
+          }}
+        >
+          {menu.length > 0
+            ? menu.map((menuItem) => (
+                <NavItemView {...{ ...menuItem, depth: 0, key: menuItem.ID }} />
+              ))
+            : null}
+        </nav>
+      )}
+    </>
+  );
 };
 
-export default MobileNavMenu;
+export default MobileNavView;
