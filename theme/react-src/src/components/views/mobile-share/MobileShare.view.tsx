9a5b92905af4541a0af4cb28dcb0e7ab0f1bd693
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-03 16:29:23 +0000

    Feat: Implement Share

--- theme/react-src/src/components/views/mobile-share/MobileShare.view.tsx
+++ theme/react-src/src/components/views/mobile-share/MobileShare.view.tsx
@@ -1,7 +1,113 @@
 import React from 'react';
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+} from 'react-share';
+import { selectSingular } from '../../../slices/singular/singular.slice';
+import { useSelector } from 'react-redux';
+
+const iconProps = {
+  size: 40,
+  style: {
+    borderRadius: 'var(--spacing)',
+  },
+};
+
+const buttonProps = {
+  style: {
+    display: 'grid',
+    gridTemplateColumns: `${iconProps.size}px 1fr`,
+    alignItems: 'center',
+    marginTop: 'var(--spacing)',
+    marginBottom: 'var(--spacing)',
+    gridColumnGap: 'var(--spacing)',
+  },
+};
 
 const MobileShareView = () => {
-  return <div>This will be the share menu</div>;
+  const { titleSeparator } = window.config;
+  const url = window.location.href;
+  const singular = useSelector(selectSingular);
+
+  const title = document.title;
+  const excerpt = singular.render
+    ? singular.excerpt
+    : `Profesyonel Koç Başak Beykoz'un internet sitesi`;
+
+  return (
+    <ol
+      style={{
+        listStyle: 'none',
+        margin: 0,
+        marginBlockStart: 0,
+        marginBlockEnd: 0,
+        marginInlineStart: 0,
+        marginInlineEnd: 0,
+        paddingInlineStart: 0,
+      }}
+    >
+      <li>
+        <FacebookShareButton
+          {...{
+            url,
+            quote: title,
+            ...buttonProps,
+          }}
+        >
+          <FacebookIcon {...iconProps} />
+          Facebook
+        </FacebookShareButton>
+      </li>
+
+      <li>
+        <TwitterShareButton
+          {...{
+            url,
+            title,
+            ...buttonProps,
+          }}
+        >
+          <TwitterIcon {...iconProps} />
+          Twitter
+        </TwitterShareButton>
+      </li>
+
+      <li>
+        <WhatsappShareButton
+          {...{
+            url,
+            title,
+            separator: titleSeparator,
+            ...buttonProps,
+          }}
+        >
+          <WhatsappIcon {...iconProps} />
+          WhatsApp
+        </WhatsappShareButton>
+      </li>
+
+      <li>
+        <LinkedinShareButton
+          {...{
+            source: url,
+            url,
+            title,
+            summary: excerpt,
+            ...buttonProps,
+          }}
+        >
+          <LinkedinIcon {...iconProps} />
+          LinkedIn
+        </LinkedinShareButton>
+      </li>
+    </ol>
+  );
 };
 
 export default MobileShareView;
