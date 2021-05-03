8ab24a8b599eb90eaf3dae13359bee8a6b5bf76c
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-03 05:39:39 +0000

    Update: footer layout
    * Minot styling update for footer for setting up the skeleton of the app

--- theme/react-src/src/components/layouts/footer/Footer.layout.tsx
+++ theme/react-src/src/components/layouts/footer/Footer.layout.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
+const FOOTER_HEIGHT = 150;
+
 const FooterLayout = () => {
   return (
     <footer
-      style={{ height: 150, backgroundColor: '#eee', textAlign: 'center' }}
+      style={{
+        height: FOOTER_HEIGHT,
+        position: 'relative',
+        textAlign: 'center',
+      }}
     >
-      Footer
+      <div
+        style={{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          bottom: 0,
+          height: FOOTER_HEIGHT,
+          backgroundColor: '#eee',
+        }}
+      >
+        Footer
+      </div>
     </footer>
   );
 };
