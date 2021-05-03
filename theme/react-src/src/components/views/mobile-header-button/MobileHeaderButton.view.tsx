ecb235c1b89e2803be45aa8432e694b71684f842
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-03 14:17:42 +0000

    Update: add state scan for header buttons
    * Now the header buttons know when they are enabled.
    
    * Minor styling changes

--- theme/react-src/src/components/views/mobile-header-button/MobileHeaderButton.view.tsx
+++ theme/react-src/src/components/views/mobile-header-button/MobileHeaderButton.view.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import type { FC } from 'react';
+import { useSelector } from 'react-redux';
 
 interface MobileHeaderButtonViewProps {
   onClick: () => void;
+  selector: (state: any) => boolean;
 }
 
 const MobileHeaderButtonView: FC<MobileHeaderButtonViewProps> = ({
   onClick,
   children,
+  selector,
 }) => {
+  const selected = useSelector(selector);
+
   return (
     <button
       onClick={onClick}
       style={{
         border: 0,
-        background: 'transparent',
+        background: selected ? 'var(--brush-darkYellow)' : 'transparent',
+        padding: 0,
+        margin: 0,
+        borderBottomLeftRadius: selected ? 'var(--spacing)' : 0,
+        borderBottomRightRadius: selected ? 'var(--spacing)' : 0,
       }}
     >
       {children}
