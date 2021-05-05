5143c0cbc890fd92ab2e6438483506c84110cfff
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-05 10:15:55 +0000

    Update: NavDesktopItem styling, active ind
    * Active indication for NavDesktopItem has been created. However
    the absolutely placed item appears in front of the menu title and
    this should be fixed in the following commits.
    
    * Hover colors for the menu button and the child items have been set.
    
    * A graphic for the active state of the menu items has been added to
    static.

--- theme/react-src/src/components/views/nav-desktop-item/NavDesktopChildItems.view.tsx
+++ theme/react-src/src/components/views/nav-desktop-item/NavDesktopChildItems.view.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { FC } from 'react';
 import { WpMenuItem } from '../../../@types/wp-types';
 import { NavDesktopItemView } from './NavDesktopItem.view';
+import { MENU_ITEM_HOVERED_COLOR, MENU_BORDER_RADIUS } from '../../../config';
 
 type NavChildItemsViewProps = {
   children: WpMenuItem[];
   depth: number;
 };
-export const NavDesktopChildItemsView: FC<NavChildItemsViewProps> = ({
+
+const NavDesktopChildItemsView: FC<NavChildItemsViewProps> = ({
   children,
   depth,
 }) => (
   <div
     style={{
-      position: 'absolute',
+      position: depth > 1 ? 'relative' : 'absolute',
+      backgroundColor: MENU_ITEM_HOVERED_COLOR,
+      borderTopRightRadius: MENU_BORDER_RADIUS,
+      borderBottomLeftRadius: MENU_BORDER_RADIUS,
+      borderBottomRightRadius: MENU_BORDER_RADIUS,
+      paddingTop: depth > 1 ? 0 : 'var(--spacing)',
+      paddingBottom: 'var(--spacing)',
+      width: depth > 1 ? '100%' : 'auto',
     }}
   >
     {children.map((item) => (
@@ -21,3 +30,5 @@ export const NavDesktopChildItemsView: FC<NavChildItemsViewProps> = ({
     ))}
   </div>
 );
+
+export default NavDesktopChildItemsView;
--- theme/react-src/src/components/views/nav-desktop-item/NavDesktopItem.view.tsx
+++ theme/react-src/src/components/views/nav-desktop-item/NavDesktopItem.view.tsx
@@ -3,10 +3,17 @@ import { FC } from 'react';
 import { useState } from 'react';
 import { WpMenuItem } from '../../../@types/wp-types';
 import { urlSlug } from '../../../utils/slug.util';
-import { NavDesktopChildItemsView } from './NavDesktopChildItems.view';
+import NavDesktopChildItemsView from './NavDesktopChildItems.view';
 import { LinkNavView } from '../link-nav/LinkNav.view';
+import { useLocation } from 'react-router-dom';
+import { ReactComponent as Stars } from '../../../static/nav-desktop-stars.svg';
+import ConcaveCornerView from '../concave-corner/ConcaveCorner.view';
+import { MENU_ITEM_HOVERED_COLOR, MENU_BORDER_RADIUS } from '../../../config';
 
 type NavDesktopItemViewProps = WpMenuItem & { depth: number };
+
+const MENU_ITEM_HORIZONTAL_SPACING = 'calc(var(--spacing) * 2)';
+const MENU_ITEM_VERTICAL_SPACING_DEEP = 'calc(var(--spacing) * 1.5)';
 
 export const NavDesktopItemView: FC<NavDesktopItemViewProps> = ({
   title,
@@ -14,28 +21,80 @@ export const NavDesktopItemView: FC<NavDesktopItemViewProps> = ({
   child_items,
   depth,
 }) => {
-  const [hovered, setHovered] = useState(false);
+  const location = useLocation();
+  let [hovered, setHovered] = useState(false);
   const urlfulSlug = urlSlug(slug);
+  const hasChildren = child_items && child_items.length > 0;
 
   return (
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
-      <LinkNavView urlfulSlug={urlfulSlug}>
-        <h4
-          style={{
-            // marginTop: 3,
-            marginLeft: 8,
-            marginRight: 8,
-          }}
-        >
-          {title}
-        </h4>
-      </LinkNavView>
+      <div
+        style={{
+          position: 'relative',
+          display: 'inline-block',
+          backgroundColor: hovered ? MENU_ITEM_HOVERED_COLOR : 'transparent',
+          borderTopLeftRadius: MENU_BORDER_RADIUS,
+          borderTopRightRadius: MENU_BORDER_RADIUS,
+          borderBottomRightRadius: hasChildren ? 0 : MENU_BORDER_RADIUS,
+          borderBottomLeftRadius: hasChildren ? 0 : MENU_BORDER_RADIUS,
+        }}
+      >
+        <LinkNavView urlfulSlug={urlfulSlug}>
+          <h4
+            style={{
+              marginTop: depth > 0 ? MENU_ITEM_VERTICAL_SPACING_DEEP : '',
+              marginBottom: depth > 0 ? MENU_ITEM_VERTICAL_SPACING_DEEP : '',
+              marginLeft:
+                depth > 1
+                  ? `calc(${MENU_ITEM_HORIZONTAL_SPACING} * ${depth})`
+                  : MENU_ITEM_HORIZONTAL_SPACING,
+              marginRight: MENU_ITEM_HORIZONTAL_SPACING,
+            }}
+          >
+            {title}
+          </h4>
 
-      {hovered && child_items && child_items.length > 0 && (
-        <NavDesktopChildItemsView children={child_items} depth={depth} />
+          {urlfulSlug === location.pathname && (
+            <Stars
+              fill="red"
+              style={{
+                position: 'absolute',
+                top: '50%',
+                left: depth === 0 ? '50%' : 40,
+                height: 50,
+                width: 70,
+                color: 'red',
+                stroke: 'pink',
+                fill: 'var(--brush-darkYellow)',
+                transform: 'translate(-50%, -50%)',
+                pointerEvents: 'none',
+                // opacity: 0.6,
+                zIndex: -1,
+              }}
+            />
+          )}
+
+          {hovered && hasChildren && (
+            <ConcaveCornerView
+              fill={MENU_ITEM_HOVERED_COLOR}
+              horizontalDirection="left"
+              verticalDirection="up"
+              style={{
+                position: 'absolute',
+                bottom: 0,
+                right: `calc(${MENU_BORDER_RADIUS} * -1)`,
+                height: MENU_BORDER_RADIUS,
+              }}
+            />
+          )}
+        </LinkNavView>
+      </div>
+
+      {hovered && hasChildren && (
+        <NavDesktopChildItemsView children={child_items} depth={depth + 1} />
       )}
     </div>
   );
