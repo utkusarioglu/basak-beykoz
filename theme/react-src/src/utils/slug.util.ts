792d51df6c03c6edd32a5487817d538058682ad6
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-05 17:25:54 +0000

    Update: refactor slug.util
    * This refactoring is done to make the util functions more predictable.

--- theme/react-src/src/utils/slug.util.ts
+++ theme/react-src/src/utils/slug.util.ts
@@ -1,6 +1,6 @@
 import { HOME_SLUG } from '../config';
 
-type StringIdentityFunc = (slug: string) => string;
+type StringIdentityFunc = (slug: string | undefined | null) => string;
 
 /**
  * Converts an empty or undefined slug to one with a slash at the end
@@ -8,7 +8,7 @@ type StringIdentityFunc = (slug: string) => string;
  * @returns address bar routing compatible slug
  */
 export const urlSlug: StringIdentityFunc = (slug) =>
-  (slug !== undefined && slug !== '' ? `/${slug}/` : '/').replace('//', '/');
+  stringNotEmpty(slug) ? `/${cleanSlug(slug)}/` : '/';
 
 /**
  * Converts raw slug to values that the rest api would understand.
@@ -18,10 +18,7 @@ export const urlSlug: StringIdentityFunc = (slug) =>
  * @returns rest api compatible slug
  */
 export const restSlug: StringIdentityFunc = (slug) =>
-  (slug !== undefined && slug !== '' && slug !== '/'
-    ? `/${slug}`
-    : `/${HOME_SLUG}`
-  ).replace('//', '/');
+  stringNotEmpty(slug) ? `/${cleanSlug(slug)}` : `/${HOME_SLUG}`;
 
 /**
  * Removes the preceding slash from the given slug, it also converts
@@ -30,6 +27,10 @@ export const restSlug: StringIdentityFunc = (slug) =>
  * @returns slug that could be used for state comparison
  */
 export const cleanSlug: StringIdentityFunc = (rawSlug) => {
-  const slashlessSlug = (rawSlug || '').split('/').join('');
-  return rawSlug === undefined ? HOME_SLUG : slashlessSlug;
+  return stringNotEmpty(rawSlug)
+    ? (rawSlug as string).split('/').join('')
+    : HOME_SLUG;
 };
+
+const stringNotEmpty = (slug: string | null | undefined): boolean =>
+  slug !== undefined && slug !== null && slug !== '' && slug !== '/';
