2caf2a138ae383410f20395c0a157145cf29614d
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-18 14:36:18 +0000

    Feat: add injection function for correcting links
    * There are a few occasions where the wordpress links don't make much
    ux sense. One of these is with the wp latest-posts block where only
    the title of the li is a link. This confuses the users.
    
    The added function will ensure that click anwhere withing a specified
    parent component will trigger the `a` click action.

---
+++ theme/react-src/src/injections/attachAnchorClickCorrection.ts
@@ -0,0 +1,42 @@
+import { WrappedInjectionFunction } from '../services/injection.service';
+import injection from '../services/injection.service';
+
+/**
+ * Allows non-link components parents of an anchor to act as links.
+ *
+ * @remarks
+ * There are some limitations in wordpress that hinder UX. One of these is
+ * wp's in ability to wrap groups in `a` tag. Which means, that it's not
+ * possible to create a link card.
+ *
+ * One other place this comes up with is the `wp-block-latest-posts`. The
+ * fact that only the title of the post is a link hinders ux, by confusing the
+ * user.
+ *
+ * This function takes a parent element and makes sure that any touch or click
+ * within the element triggers the `a` tag inside the parent. For example, for
+ * wp-block-latest-posts, this parent is the `li` element.
+ *
+ * @param param0 elem for the html element that contains the
+ * wordpress list items
+ * @returns () => injection function
+ */
+export const attachAnchorClickCorrection: WrappedInjectionFunction<{
+  elem: HTMLElement;
+  parentSelector: string;
+}> = ({ elem, parentSelector }) => {
+  const parents = elem.querySelectorAll(parentSelector);
+
+  const handler = (e: any) => {
+    const closest = e.target.closest(parentSelector);
+    const a = closest?.querySelector('a');
+    a?.click();
+  };
+
+  return () =>
+    injection.unmountableEventListener(
+      Array.from(parents) as HTMLElement[],
+      ['click', 'touch'],
+      handler
+    );
+};
