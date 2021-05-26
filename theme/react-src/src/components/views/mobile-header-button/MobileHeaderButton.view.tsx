de38cdd4dcb5344a2495887b8e8a47ddcfdf694a
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-26 14:20:05 +0000

    Update: responsive MovileHeaderButton.view
    * This also features a fix div, which is a hacky solution to deal with
    changing border radii rendering the background components
    visible - where none should. Please refer to the comment section
    that starts with `TODO` in the module to learn more about this.

--- theme/react-src/src/components/views/mobile-header-button/MobileHeaderButton.view.tsx
+++ theme/react-src/src/components/views/mobile-header-button/MobileHeaderButton.view.tsx
@@ -6,6 +6,7 @@ import {
   MENU_DECORATION_COLOR,
   MENU_VERTICAL_DECORATION_HEIGHT,
 } from '../../../config';
+import { useResponsiveWidth } from '../../../utils/responsive.util';
 
 interface MobileHeaderButtonViewProps {
   onClick: () => void;
@@ -13,8 +14,6 @@ interface MobileHeaderButtonViewProps {
   listPosition: number;
   listLength: number;
 }
-
-const DECORATION_SIZE = 'var(--sp)';
 
 const MobileHeaderButtonView: FC<MobileHeaderButtonViewProps> = ({
   onClick,
@@ -25,6 +24,7 @@ const MobileHeaderButtonView: FC<MobileHeaderButtonViewProps> = ({
 }) => {
   const selected = useSelector(selector);
   const isLast = listPosition === listLength - 1;
+  const isW = useResponsiveWidth();
 
   return (
     <div
@@ -34,33 +34,75 @@ const MobileHeaderButtonView: FC<MobileHeaderButtonViewProps> = ({
     >
       {selected && (
         // You could need an "!isFirst" check above if the
-        // lists weren't all facing right
-        <ConcaveCornerView
-          fill={MENU_DECORATION_COLOR}
-          horizontalDirection="left"
-          verticalDirection="down"
-          style={{
-            width: DECORATION_SIZE,
-            height: DECORATION_SIZE,
-            position: 'absolute',
-            top: MENU_VERTICAL_DECORATION_HEIGHT,
-            left: `calc(${DECORATION_SIZE} * -1)`,
-            zIndex: 115,
-          }}
-        />
+        // lists weren't all facing left
+        <>
+          <ConcaveCornerView
+            className="has-responsive-size-for-decoration-block"
+            fill={MENU_DECORATION_COLOR}
+            horizontalDirection="left"
+            verticalDirection="down"
+            style={{
+              position: 'absolute',
+              top: MENU_VERTICAL_DECORATION_HEIGHT,
+              zIndex: 115,
+              left: 'calc(var(--sp) * -1)',
+
+              ...(isW.xs && {
+                left: 'calc(var(--sp) * -1 * 1.5)',
+              }),
+
+              ...(isW.sm && {
+                left: 'calc(var(--sp) * -1 * 2)',
+              }),
+            }}
+          />
+          <div
+            /**
+             * TODO
+             *
+             * This is my _div of shame_
+             * when the breakpoint reaches md, the selected header buttons
+             * start to show gap between all the border radius that is
+             * going on all over the place. This shameful div is there
+             * to cover some ugliness.
+             *
+             * There are better solutions to this that involve increasing the
+             * height of the menu buttons when one is selected. But these
+             * require some rewrites that can make this project go overtime.
+             *
+             * This solution will hold fine, even though it's an ugly and
+             * shameful hack.
+             */
+            className="has-responsive-right-for-blocks"
+            style={{
+              position: 'fixed',
+              bottom: `calc(var(--height-header-mobile) - ${MENU_VERTICAL_DECORATION_HEIGHT})`,
+              width: 20,
+              height: 20,
+              backgroundColor: isLast
+                ? 'var(--brush-yellow-dark)'
+                : 'var(--brush-white)',
+            }}
+          />
+        </>
       )}
 
       <button
+        className="has-responsive-border-radius"
         onClick={onClick}
         style={{
           border: 0,
-          background: selected ? MENU_DECORATION_COLOR : 'transparent',
           padding: 0,
           margin: 0,
           height: '100%',
           width: '100%',
-          borderBottomLeftRadius: selected ? 'var(--sp)' : 0,
-          borderBottomRightRadius: selected ? 'var(--sp)' : 0,
+          background: 'transparent',
+          borderTopRightRadius: 0,
+          borderTopLeftRadius: 0,
+
+          ...(selected && {
+            background: MENU_DECORATION_COLOR,
+          }),
         }}
       >
         {children}
@@ -68,16 +110,23 @@ const MobileHeaderButtonView: FC<MobileHeaderButtonViewProps> = ({
 
       {selected && !isLast && (
         <ConcaveCornerView
+          className="has-responsive-size-for-decoration-block"
           fill={MENU_DECORATION_COLOR}
           horizontalDirection="right"
           verticalDirection="down"
           style={{
-            width: DECORATION_SIZE,
-            height: DECORATION_SIZE,
             position: 'absolute',
             top: 'var(--sp)',
-            right: `calc(${DECORATION_SIZE} * -1)`,
             zIndex: 115,
+            right: `calc(var(--sp) * -1)`,
+
+            ...(isW.xs && {
+              right: 'calc(var(--sp) * -1 * 1.5)',
+            }),
+
+            ...(isW.sm && {
+              right: 'calc(var(--sp) * -1 * 2)',
+            }),
           }}
         />
       )}
