c1bba80dd1eb6fb15daf525bfaa4ce49eeaf7d7c
utkusarioglu <utkusarioglu@hotmail.com>
2021-05-26 14:17:37 +0000

    Update: responsive Standard.layout
    * The component has been updated to be responsive. Css based responsive
    classes work in tandem with js based alterations that are unique
    to this component.
    
    * A new div has been wrapped around the `children` content. This div
    is responsible for handling thumbnail image presence. This allows the
    inner div to use css helper classes for responsive padding.

--- theme/react-src/src/components/layouts/standard/Standard.layout.tsx
+++ theme/react-src/src/components/layouts/standard/Standard.layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import type { FC } from 'react';
-import { useMediaQuery } from 'react-responsive';
-import { W_MD } from '../../../config';
+import { useResponsiveWidth } from '../../../utils/responsive.util';
 
 const THUMBNAIL_WIDTH_DESKTOP = '30vw';
 
@@ -25,126 +24,149 @@ const StandardLayout: FC<StandardLayoutProps> = ({
   hideFooterShim = false,
   hideHeaderShim = false,
 }) => {
+  const isW = useResponsiveWidth();
   const isWMd = useMediaQuery({ minWidth: W_MD });
 
   return (
     <>
-      {!hideThumbnail && (
-        <div
-          style={
-            isWMd
-              ? {
-                  position: 'fixed',
-                  left: 0,
-                  bottom: 0,
-                  top: 0,
-                  width: THUMBNAIL_WIDTH_DESKTOP,
-                }
-              : {
-                  position: 'relative',
-                  width: '100%',
-                  height: '40vh',
-                }
-          }
-        >
+      {
+        /**
+         * The thumbnail block
+         */
+        !hideThumbnail && (
           <div
             style={{
-              backgroundColor: 'var(--brush-green-light)',
-              ...(thumbnailUrl && {
-                backgroundImage: `url(${thumbnailUrl})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundPositionX: 'center',
-                backgroundPositionY: 'center',
-                backgroundSize: 'cover',
-                filter: 'sepia(50%) opacity(.5)',
+              ...(!isW.md && {
+                position: 'relative',
+                width: '100%',
+                height: '40vh',
               }),
-              width: '100%',
-              height: '100%',
-              position: 'relative',
+
+              ...(isW.md && {
+                position: 'fixed',
+                left: 0,
+                bottom: 0,
+                top: 0,
+                width: THUMBNAIL_WIDTH_DESKTOP,
+              }),
             }}
-          />
-          {!isWMd && (
+          >
             <div
               style={{
+                /**
+                 * This styling arrangement ensures that this section
+                 * doesn't have any filters applied if there is no image
+                 * to show.
+                 */
+                backgroundColor: 'var(--brush-green-light)',
+                ...(thumbnailUrl && {
+                  backgroundImage: `url(${thumbnailUrl})`,
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPositionX: 'center',
+                  backgroundPositionY: 'center',
+                  backgroundSize: 'cover',
+                  filter: 'sepia(50%) opacity(.5)',
+                }),
                 width: '100%',
-                height: 16,
-                backgroundColor: 'var(--brush-white)',
-                position: 'absolute',
-                bottom: -8,
-                borderRadius: 8,
+                height: '100%',
+                position: 'relative',
               }}
             />
-          )}
 
-          {!hideTitle && !isWMd && (
-            <div
-              style={{
-                position: 'absolute',
-                bottom: 0,
-                textAlign: 'center',
-                width: '100%',
-              }}
-            >
-              <h1
+            {
+              /**
+               * renders a decoration div that makes the edges of the
+               * thumbnail curved. This has an analogue in css through a
+               * mixin called `section-top-decoration`
+               */
+              !isW.md && (
+                <div
+                  style={{
+                    width: '100%',
+                    height: 16,
+                    backgroundColor: 'var(--brush-white)',
+                    position: 'absolute',
+                    bottom: -8,
+                    borderRadius: 8,
+                  }}
+                />
+              )
+            }
+
+            {isW.lg && <LazyShareDesktop />}
+
+            {!hideTitle && !isW.md && (
+              <div
                 style={{
-                  paddingLeft: 'var(--sp)',
-                  paddingRight: 'var(--sp)',
-                  /**
-                   * the client plans to use very long page titles h1
-                   * is way too big for small mobile screens this sizing
-                   * makes h1 be the same size as h2 the distinction in
-                   * title hierarchy of h1 and h2 is made through the placement
-                   * of h1 over the page thumbnail
-                   */
-                  fontSize: '1.5em',
+                  position: 'absolute',
+                  bottom: 0,
+                  textAlign: 'center',
+                  width: '100%',
                 }}
               >
-                {title}
-              </h1>
-            </div>
-          )}
-        </div>
-      )}
+                <h1
+                  style={{
+                    paddingLeft: 'var(--sp)',
+                    paddingRight: 'var(--sp)',
+                    /**
+                     * the client plans to use very long page titles h1
+                     * is way too big for small mobile screens this sizing
+                     * makes h1 be the same size as h2 the distinction in
+                     * title hierarchy of h1 and h2 is made through the placement
+                     * of h1 over the page thumbnail
+                     */
+                    fontSize: '1.5em',
+                  }}
+                >
+                  {title}
+                </h1>
+              </div>
+            )}
+          </div>
+        )
+      }
 
       <div
         style={{
-          ...(hideThumbnail
-            ? isWMd // TODO these need to be created
-              ? {}
-              : {}
-            : isWMd
-            ? {
-                paddingTop: '10vh', // comes from heading and some whitespace creation
-                marginLeft: `calc(${THUMBNAIL_WIDTH_DESKTOP} + var(--header-desktop-horizontal-padding))`,
-                marginRight: 'var(--header-desktop-horizontal-padding)',
-                minHeight: 'calc(100% - 10vh)',
-              }
-            : {
-                marginTop: 'var(--sp)',
-                marginLeft: computeVerticalMargins(hideMargins, isWMd),
-                marginRight: computeVerticalMargins(hideMargins, isWMd),
-              }),
-
-          ...(!hideFooterShim &&
-            (isWMd
-              ? {
-                  paddingBottom: 'var(--height-footer-shim-desktop)',
-                  minHeight: 'calc(100vh + var(--height-footer-desktop))',
-                }
-              : {
-                  paddingBottom: 'var(--height-footer-shim-mobile)',
-                  /**
-                   * TODO
-                   * this doesn't account for the mobile browser address
-                   * bar scroll behavior.
-                   *
-                   * It also doesn't account for the cases where the
-                   * thumbnail may be turned off
-                   */
-                  minHeight: 'calc(40vh + var(--height-footer-mobile))',
-                })),
+          ...(!hideThumbnail &&
+            isW.md && {
+              marginLeft: THUMBNAIL_WIDTH_DESKTOP,
+            }),
         }}
       >
+        <div
+          className={
+            !hideMargins ? 'has-responsive-horizontal-padding-for-hero' : ''
+          }
+          style={{
+            ...(!hideThumbnail && {
+              marginTop: 'var(--sp)',
+
+              ...(isW.md && {
+                marginTop: 0, // undo small screen margin
+                minHeight: 'calc(100% - 10vh)',
+              }),
+            }),
+
+            ...(!hideFooterShim && {
+              paddingBottom: 'var(--height-footer-shim-mobile)',
+              /**
+               * TODO
+               * this doesn't account for the mobile browser address
+               * bar scroll behavior.
+               *
+               * It also doesn't account for the cases where the
+               * thumbnail may be turned off
+               */
+              minHeight: 'calc(40vh + var(--height-footer-mobile))',
+
+              ...(isW.md && {
+                paddingBottom: 'var(--height-footer-shim-desktop)',
+                minHeight: 'calc(100vh + var(--height-footer-desktop))',
+              }),
+            }),
+          }}
+        >
           {
             /**
              * The large screen header has a variable size.
@@ -160,24 +182,14 @@ const StandardLayout: FC<StandardLayoutProps> = ({
               />
             )
           }
+          {isW.md && !hideTitle && (
+            <h1 style={{ marginBottom: '2.5em', marginTop: '2em' }}>{title}</h1>
+          )}
+          {children}
+        </div>
+      </div>
     </>
   );
 };
-
-/**
- * Computes the size of the vertical margins that shall be used for the
- * component
- * @param hideMargins hide margins var from the parent
- * @param isWMd boolean indicating whether the desktop breakpoint has
- * been reached
- * @returns boolean - true to hide the vertical margins
- */
-function computeVerticalMargins(hideMargins: boolean, isWMd: boolean) {
-  return hideMargins
-    ? 0
-    : !isWMd
-    ? 'var(--sp)'
-    : 'var(--header-desktop-horizontal-padding)';
-}
 
 export default StandardLayout;
