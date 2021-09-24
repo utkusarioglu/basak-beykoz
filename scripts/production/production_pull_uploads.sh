316f6e7781234cc68d5e204fe4e9028978cb9b1e
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-09-24 07:05:39 +0000

    Add folder creation logic to prod pull uploads

--- scripts/production/production_pull_uploads.sh
+++ scripts/production/production_pull_uploads.sh
@@ -8,11 +8,19 @@ source scripts/shared/exit_if_devcontainer.sh
 
 check_ownership "$HOST_UPLOADS_DIR"
 
-PROD_UPLOADS_BACKUP_DIR=$REMOTE_CONTENT_DIR/wp-content/uploads
+PROD_UPLOADS_BACKUP_DIR=$REMOTE_PROJECT_DIR/wp-content/uploads
 HOST_UPLOADS_BACKUP_DIR=$PWD/$HOST_UPLOADS_DIR
 
 echo 'Clearing current uploads'
+mkdir -p uploads
 rm -rf $HOST_UPLOADS_DIR/*
+
+echo "Creating folders if needed"
+gcloud beta compute ssh \
+  --zone "$ZONE" \
+  "$VM" \
+  --project "$PROJECT" \
+  --command "mkdir -p $PROD_UPLOADS_BACKUP_DIR"
 
 echo "Downloading production uploads to \"$HOST_UPLOADS_DIR\"..."
 gcloud compute scp \
